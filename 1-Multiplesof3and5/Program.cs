dccaae93f5db6d3efe33310201657d30a5794b23
Thomas Holmes <ThomasHolmes@outlook.com>
2015-06-04 12:57:57 +0100

    Solved problem 1.

---
+++ 1-Multiplesof3and5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Multiplesof3and5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Enumerable.Range(0, 1000).Where(n => n % 3 == 0 || n % 5 == 0).Sum();
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
