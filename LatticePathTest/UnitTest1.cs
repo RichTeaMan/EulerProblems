0aa4d5a981e139b28dee8e2c9808caac0006c145
Thomas Holmes <ThomasHolmes@outlook.com>
2015-06-23 11:56:06 +0100

    Solved Lattice Paths with combinatorics.

---
+++ LatticePathTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LatticePathTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void LatticePathTest()
+        {
+            var paths = LatticePaths.Program.GetPaths(2);
+            Assert.AreEqual(6, paths);
+        }
+    }
+}
---
+++ LatticePaths/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatticePaths
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Paths: {0}", GetPaths(20));
+            Console.ReadKey();
+        }
+
+        public static long GetPaths(int gridSize)
+        {
+            long paths = 1;
+            for (int i = 0; i < gridSize; i++)
+            {
+                paths *= (2 * gridSize) - i;
+                paths /= i + 1;
+            }
+            return paths;
+        }
+    }
+}
