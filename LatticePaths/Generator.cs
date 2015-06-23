36c06ca7ac43824acd9d95b158449c7eff2da374
Thomas Holmes <ThomasHolmes@outlook.com>
2015-06-23 11:29:21 +0100

    This was an attempt to generate all seqence of a lattice grid, representing right and down movements as a list of bools.
    It did not scale well.

---
+++ LatticePaths/Generator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatticePaths
+{
+    public class Generator
+    {
+        public static IEnumerable<bool[]> Sequence(int sequenceSize)
+        {
+            int skip = 64 - sequenceSize;
+            long stop = (long)Math.Pow(2.0, sequenceSize);
+            for (long i = 0; i < stop; i++)
+            {
+                var bools = BytesToBool(BitConverter.GetBytes(i));
+                bools = bools.Take(sequenceSize).ToArray();
+                yield return bools;
+            }
+        }
+
+        private static bool[] BytesToBool(byte[] bytes)
+        {
+            var bools = new List<bool>();
+            foreach (var b in bytes)
+            {
+                foreach (var bitNumber in Enumerable.Range(0, 8))
+                {
+                    bools.Add((b & (1 << bitNumber)) != 0);
+                }
+            }
+            return bools.ToArray();
+        }
+    }
+}
---
+++ LatticePaths/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatticePaths
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Paths: {0}", GetPaths(20));
+            Console.ReadKey();
+        }
+
+        public static int GetPaths(int gridSize)
+        {
+            var pathCount = Generator.Sequence(gridSize * 2).Count(s => s.Count(b => b) == gridSize);
+            return pathCount;
+        }
+    }
+}
